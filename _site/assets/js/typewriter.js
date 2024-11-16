b1610eb6b02d07bd5661a3ff2c3d8411f977ea99
Qi Yu <qiyu@lap942.sprachwiss.uni-konstanz.de>
2024-11-16 17:32:07 +0100

    updated layout

--- _site/assets/js/typewriter.js
+++ _site/assets/js/typewriter.js
@@ -2,14 +2,14 @@ document.addEventListener('DOMContentLoaded', function(event) {
       var dataText = ["Full Stack Development", "Data Science", "Machine Learning", "Natural Language Processing"];
 
       function typeWriter(text, i, fnCallback) {
-        if (i < text.length) {
-          document.querySelector(".typing").innerHTML = text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
-          setTimeout(function() {
-            typeWriter(text, i + 1, fnCallback);
-          }, 100);
-        } else if (typeof fnCallback == 'function') {
-          setTimeout(fnCallback, 700);
-        }
+          if (i < text.length) {
+            document.querySelector(".typing").textContent = text.substring(0, i + 1);
+            setTimeout(function() {
+              typeWriter(text, i + 1, fnCallback);
+            }, 100);
+          } else if (typeof fnCallback == 'function') {
+            setTimeout(fnCallback, 700);
+          }
       }
 
       function StartTextAnimation(i) {
